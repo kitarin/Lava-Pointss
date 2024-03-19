5366bf169d11a971fdfc5ed24b0c31d66fd6c1e7
kitarin <85457729+kitarin@users.noreply.github.com>
2024-03-20 02:59:49 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://rpc.starknet-testnet.lava.build/lava-referer-c732f1ab-5e7e-4a83-9ce9-1602a1e09a5e/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c732f1ab-5e7e-4a83-9ce9-1602a1e09a5e/)
 
 
 echo "$curl_output" >> Update.tsx
